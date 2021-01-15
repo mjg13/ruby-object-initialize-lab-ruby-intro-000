11c021dc3703898aafa3e81b8d1ed6aadc452185
Mary Jo Gray <mj81000@gmail.com>
2021-01-15 17:46:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,16 @@
 class Dog
 
-  def initialize(breed="Mutt")
+  def initialize(name, breed="Mutt")
+    @name = name
     @breed = breed
+  end
+
+  def name
+    @name
+  end
+
+  def breed
+    @breed
   end
 
 end
